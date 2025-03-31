e689dff224dd7318d24dac2179d293c041be4b93
Ubuntu <ubuntu@ip-172-31-0-12.eu-north-1.compute.internal>
2025-03-31 11:19:19 +0000

    changes in testing file

--- testing.py
+++ testing.py
@@ -1,4 +1,8 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-sum = a + b
-(print("The sum is:", sum))
+"""
+A simple script to add two numbers.
+"""
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+total = a + b
+print("The sum is:", total)
